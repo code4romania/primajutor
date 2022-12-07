2f7847389862541fee4e879be3b521e3adbd3c15
Andrei Ioniță <hi@andrei.io>
2022-12-07 16:03:41 +0000

    add database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,9 +1,14 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Database\Seeders;
 
+use App\Models\User;
+use BezhanSalleh\FilamentShield\Commands\MakeShieldGenerateCommand;
+use BezhanSalleh\FilamentShield\Commands\MakeShieldSuperAdminCommand;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Artisan;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +19,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        Artisan::call(MakeShieldGenerateCommand::class, [
+            '--all' => true,
+        ]);
 
+        $user = User::factory(['email' => 'admin@example.com'])
+            ->create();
+
+        Artisan::call(MakeShieldSuperAdminCommand::class, [
+            '--user' => $user->id,
+        ]);
     }
 }
