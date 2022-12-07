526153656ff7e9de33085ee811f1e66e5420fe32
Andrei Ioniță <hi@andrei.io>
2022-12-07 19:31:42 +0000

    help course layout

--- app/Filament/Resources/HelpCourseResource.php
+++ app/Filament/Resources/HelpCourseResource.php
@@ -1,26 +1,20 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Filament\Resources;
 
 use App\Filament\Resources\HelpCourseResource\Pages;
-use App\Filament\Resources\HelpCourseResource\RelationManagers;
 use App\Models\City;
 use App\Models\County;
 use App\Models\HelpCourse;
 use Filament\Forms;
-use Filament\Forms\Components\DatePicker;
-use Filament\Forms\Components\Select;
-use Filament\Forms\Components\TextInput;
 use Filament\Resources\Concerns\Translatable;
 use Filament\Resources\Form;
 use Filament\Resources\Resource;
 use Filament\Resources\Table;
 use Filament\Tables;
-use Filament\Tables\Columns\TextColumn;
-use Filament\Tables\Filters\Layout;
-use Filament\Tables\Filters\SelectFilter;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Contracts\Database\Eloquent\Builder;
 
 class HelpCourseResource extends Resource
 {
@@ -34,27 +28,44 @@ class HelpCourseResource extends Resource
     {
         return $form
             ->schema([
-                TextInput::make('title')->required(),
-                Select::make('county_id')
-                    ->label('County')
-                    ->options(County::all()->pluck('name', 'id')->toArray())
-                    ->required()
-                    ->reactive()
-                    ->searchable()
-                    ->afterStateUpdated(fn (callable $set) => $set('city_id', null)),
-                Select::make('city_id')
-                    ->label('City')
-                    ->required()
-                    ->options(
-                        fn (callable $get) => County::find($get('county_id'))
-                            ?->cities
-                            ->pluck('name', 'id')
-                    )
-                    ->searchable()
-                    ->reactive(),
-                TextInput::make('info')->required(),
-                TextInput::make('link')->url()->required(),
-                DatePicker::make('date')->required(),
+                Forms\Components\Card::make()
+                    ->schema([
+                        Forms\Components\TextInput::make('title')
+                            ->required()
+                            ->columnSpanFull(),
+
+                        Forms\Components\TextInput::make('info')
+                            ->required()
+                            ->columnSpanFull(),
+
+                        Forms\Components\Select::make('county_id')
+                            ->label('County')
+                            ->options(County::pluck('name', 'id'))
+                            ->required()
+                            ->reactive()
+                            ->searchable()
+                            ->afterStateUpdated(fn (callable $set) => $set('city_id', null)),
+
+                        Forms\Components\Select::make('city_id')
+                            ->label('City')
+                            ->required()
+                            ->options(
+                                fn (callable $get) => County::find($get('county_id'))
+                                    ?->cities
+                                    ->pluck('name', 'id')
+                            )
+                            ->searchable()
+                            ->reactive(),
+
+                        Forms\Components\TextInput::make('link')
+                            ->url()
+                            ->required(),
+
+                        Forms\Components\DatePicker::make('date')
+                            ->required(),
+                    ])
+                    ->columns(2),
+
             ]);
     }
 
@@ -62,32 +73,34 @@ class HelpCourseResource extends Resource
     {
         return $table
             ->columns([
-                TextColumn::make('title'),
-                TextColumn::make('date'),
-                TextColumn::make('county_id')
+                Tables\Columns\TextColumn::make('title'),
+                Tables\Columns\TextColumn::make('date'),
+                Tables\Columns\TextColumn::make('county_id')
                     ->label('County')
                     ->formatStateUsing(function (string $state) {
                         $county = County::find($state);
+
                         return (string) $county->name;
                     }),
-                TextColumn::make('city_id')
+                Tables\Columns\TextColumn::make('city_id')
                     ->label('City')
                     ->formatStateUsing(function (string $state) {
                         $city = City::find($state);
+
                         return (string) $city->name;
                     }),
             ])
-            ->filters([
-                    SelectFilter::make('county_id')
-                        ->label('County')
-                        ->options(County::whereHas('helpCourses')->pluck('name', 'id')->toArray())
+            ->filters(
+                [
+                    Tables\Filters\SelectFilter::make('county')
+                        ->relationship('county', 'name', fn (Builder $query) => $query->whereHas('helpCourses'))
                         ->searchable(),
-                    SelectFilter::make('city_id')
-                        ->label('City')
-                        ->options(City::whereHas('helpCourses')->pluck('name', 'id')->toArray())
-                        ->searchable()
+
+                    Tables\Filters\SelectFilter::make('city')
+                        ->relationship('city', 'name', fn (Builder $query) => $query->whereHas('helpCourses'))
+                        ->searchable(),
                 ],
-                layout: Layout::AboveContent,
+                layout: Tables\Filters\Layout::AboveContent,
             )
             ->actions([
                 Tables\Actions\EditAction::make(),
