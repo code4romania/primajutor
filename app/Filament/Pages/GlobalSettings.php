427c251c87f5dad2e1c968223fd15cfdf37939f2
Andrei Ioniță <hi@andrei.io>
2022-12-07 19:30:17 +0000

    refactor: reorganize global settings page layout

--- app/Filament/Pages/GlobalSettings.php
+++ app/Filament/Pages/GlobalSettings.php
@@ -1,11 +1,12 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Filament\Pages;
 
 use App\Settings\GlobalSettings as SpatieGlobalSettings;
 use BezhanSalleh\FilamentShield\Traits\HasPageShield;
-use Filament\Forms\Components\Textarea;
-use Filament\Forms\Components\TextInput;
+use Filament\Forms;
 use Filament\Pages\SettingsPage;
 
 class GlobalSettings extends SettingsPage
@@ -19,20 +20,31 @@ class GlobalSettings extends SettingsPage
     protected function getFormSchema(): array
     {
         return [
-            TextInput::make('seo_title')
-                ->required(),
-            TextArea::make('seo_keywords')
-                ->required(),
-            TextArea::make('seo_description')
-                ->required(),
-            TextInput::make('contact_email')
-                ->required(),
-            TextInput::make('facebook')
-                ->required(),
-            TextInput::make('instagram')
-                ->required(),
-            TextInput::make('youtube')
-                ->required(),
+
+            Forms\Components\Section::make('General')
+                ->schema([
+                    Forms\Components\TextInput::make('seo_title')
+                        ->required(),
+                    Forms\Components\TagsInput::make('seo_keywords')
+                        ->separator()
+                        ->required(),
+                    Forms\Components\Textarea::make('seo_description')
+                        ->required(),
+                ]),
+
+            Forms\Components\Section::make('Footer')
+                ->schema([
+                    Forms\Components\TextInput::make('contact_email')
+                        ->required()
+                        ->columnSpanFull(),
+                    Forms\Components\TextInput::make('facebook')
+                        ->required(),
+                    Forms\Components\TextInput::make('instagram')
+                        ->required(),
+                    Forms\Components\TextInput::make('youtube')
+                        ->required(),
+                ]),
+
         ];
     }
 }
