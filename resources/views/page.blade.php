daeac154c8392f738b91d0d37cdb94cf5bb5bbbd
Andrei Ioniță <hi@andrei.io>
2022-12-08 14:19:44 +0000

    refactor pages

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -1,28 +1,28 @@
 @extends('layout')
 
-@section('seo_title')  {{$page->seo_title ?? $settings->seo_title}} @endsection
-@section('seo_keywords') {{$page->seo_keywords ?? $settings->seo_keywords}} @endsection
-@section('seo_description') {{$page->seo_description ?? $settings->seo_description}} @endsection
+@section('title', $page->title)
 
 @section('content')
     <main class="app-main">
         <div class="bg-gray">
             <div class="container flex py-3 header-global-container">
-                <a href="{{route('home')}}" target="_blank" rel="noopener" class="breadcrumb-link whitespace-nowrap">
-                    {{__('txt.buttons.home')}}
-                </a> / {{ $page->title  }}
+                <a href="{{ route('home') }}" target="_blank" rel="noopener" class="breadcrumb-link whitespace-nowrap">
+                    {{ __('txt.buttons.home') }}
+                </a> / {{ $page->title }}
             </div>
         </div>
+
         <section class="text-content-section">
             <div class="container-fluid">
                 <div class="container">
-                    @if(!empty($page->media))
+                    @if (!empty($page->media))
                         <div class="tp-image-container">
-                            <div class="el-image-bckg" style="background-image: url('{{$page->getFirstMediaUrl('banner')}}')"></div>
+                            <div class="el-image-bckg"
+                                style="background-image: url('{{ $page->getFirstMediaUrl('banner') }}')"></div>
                         </div>
                     @endif
                     <div class="tp-container">
-                        <h4> {{ $page->title }} </h4>
+                        <h4>{{ $page->title }}</h4>
                         <div class="text-contente-element">
                             {!! $page->content !!}
                         </div>
@@ -31,8 +31,4 @@
             </div>
         </section>
     </main>
-@endsection
-
-@section('js')
-
 @endsection
