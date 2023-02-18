2985f3ad42f95d5705466358b88ef4f87e4e1a69
Vilen Bejenaru <70964449+vilenbejenaru@users.noreply.github.com>
2023-02-18 17:36:46 +0300

    update tw-header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,25 +1,25 @@
 <header>
-    <div class="container max-w-full ">
-        <div class="bg-bg-gray h-12">
-            <div class="container mx-auto sm:px-6 flex py-2">
-                <img src="{{ mix('assets/images/code4ro_head.png') }}" class="h-8 w-28">
-                <div class="ml-4 self-center text-base font-['Roboto']">
+    <div class="container max-w-full">
+        <div class="h-12 bg-bg-gray">
+            <div class="container mx-auto flex py-2 sm:px-6">
+                <img class="h-8 w-28" src="{{ mix('assets/images/code4ro_head.png') }}">
+                <div class="ml-4 self-center font-['Roboto'] text-base">
                     <span>{{ __('txt.header.code_for_romania') }}</span>
-                    <a href="https://code4.ro/en/donate/" target="_blank" rel="noopener"
-                        class="text-blue-600 ml-2 whitespace-nowrap">
+                    <a class="ml-2 whitespace-nowrap text-blue-600" href="https://code4.ro/en/donate/" target="_blank"
+                        rel="noopener">
                         {{ __('txt.buttons.find_more') }}
                     </a>
                 </div>
             </div>
         </div>
         <div class="container mx-auto h-24 sm:px-4">
-            <div class="h-24 flex items-center">
+            <div class="flex h-24 items-center">
                 <div class="w-40">
-                    <a href="{{ route('home') }}" class="logo-box">
+                    <a class="logo-box" href="{{ route('home') }}">
                         <img src="{{ mix('assets/images/logo.png') }}" alt="Logo">
                     </a>
                 </div>
-                <div class="block  md:hidden text-3xl cursor-pointer">
+                <div class="block cursor-pointer text-3xl md:hidden">
                     <svg class="burger-btn" id="menuTrigger" width="80" height="52" viewBox="0 0 40 26"
                         xmlns="http://www.w3.org/2000/svg">
                         <rect class="burger-btn--1" width="40" height="6" rx="3" ry="3" />
@@ -29,33 +29,36 @@
                             ry="3" />
                     </svg>
                 </div>
-                <div class="w-full flex justify-end items-center" id="sideNav">
-                    <div class="flex  items-center text-lg font-bold">
+                <div class="flex w-full items-center justify-end" id="sideNav">
+                    <div class="flex items-center text-lg font-bold">
 
                         {{-- ToDo add routes to menu  --}}
 
-                        <a href="{{ route('home') }}" class="text-lg font-bold mr-10  "> {{ __('txt.buttons.home') }} </a>
-                        <a href="{{ route('home') }}" class="text-lg font-bold mr-10"> {{ __('txt.buttons.about_project') }} </a>
-                        <a href="{{ route('home') }}" class="text-lg font-bold mr-10"> {{ __('txt.buttons.guides') }} </a>
-                        <a href="{{ route('home') }}"  class="text-lg font-bold mr-10"> {{ __('txt.buttons.courses') }} </a>
-                        <a href="{{ route('courses.index') }}" class="text-lg font-bold mr-10"> {{ __('txt.buttons.contact') }} </a>
-                        <a href="{{ route('courses.index') }}" class="text-lg font-bold mr-10"> {{ __('txt.buttons.sustain') }} </a>
-                        
+                        <a class="mr-10 text-lg font-bold" href="{{ route('home') }}"> {{ __('txt.buttons.home') }} </a>
+                        <a class="mr-10 text-lg font-bold" href="{{ route('home') }}">
+                            {{ __('txt.buttons.about_project') }} </a>
+                        <a class="mr-10 text-lg font-bold" href="{{ route('home') }}"> {{ __('txt.buttons.guides') }}
+                        </a>
+                        <a class="mr-10 text-lg font-bold" href="{{ route('home') }}"> {{ __('txt.buttons.courses') }}
+                        </a>
+                        <a class="mr-10 text-lg font-bold" href="{{ route('courses.index') }}">
+                            {{ __('txt.buttons.contact') }} </a>
+                        <a class="mr-10 text-lg font-bold" href="{{ route('courses.index') }}">
+                            {{ __('txt.buttons.sustain') }} </a>
                         @foreach ($pages as $page)
                             @if ($page->show_in_header)
                                 <a href="{{ route('page', $page) }}"> {{ $page->title }} </a>
                             @endif
                         @endforeach
-
-                     
-                        
-                        <a href="https://code4.ro/en/donate/" class="button w-32 h-10 text-white flex justify-center  items-center  bg-success-color"> {{ __('txt.buttons.donate') }} </a>
-                        
+                        <a class="button flex h-10 w-32 items-center justify-center bg-success-color text-white"
+                            href="https://code4.ro/en/donate/"> {{ __('txt.buttons.donate') }} </a>
                     </div>
                 </div>
-              
+
             </div>
         </div>
-
+        <div class="flex h-16 items-center justify-center bg-main-color">
+            <h4 class="text-3xl font-bold text-white">Dacă aveti o urgență sunați la 112</h4>
+        </div>
     </div>
 </header>
