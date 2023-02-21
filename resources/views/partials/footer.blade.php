b442a3003ccec816dd5496757ccc2c4b33e67642
Vilen Bejenaru <70964449+vilenbejenaru@users.noreply.github.com>
2023-02-21 16:13:09 +0300

    responsive header&footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,25 +1,34 @@
-<footer class="absolute bottom-0 left-0 flex w-full flex-col">
-    <div class="container mx-auto my-2 flex items-center justify-end">
-        <div class="container mx-auto flex items-center justify-end">
-            <span class="mx-4 self-end">{{ __('txt.footer.project_by') }}</span>
-            <a class="ml-3" href="https://code4.ro/ro/code-for-romania-war-task-force">
-                <img class="h-14" src="{{ mix('assets/images/code4ro_head.png') }}" alt="Code4Romania logo">
-            </a>
-            <a class="ml-3" href="https://code4.ro/ro/code-for-romania-war-task-force">
-                <img class="h-14" src="{{ mix('assets/images/logo-smurd2.png') }}" alt="SMURD logo">
-            </a>
-            <span class="ml-24 self-end">{{ __('txt.footer.supported_by') }}</span>
-            <a class="ml-3" href="https://code4.ro/ro/code-for-romania-war-task-force">
-                <img class="h-14" src="{{ mix('assets/images/lidl-logo.png') }}" alt="Lidl logo">
-            </a>
+<footer class="mx-auto flex w-full flex-col">
+    <hr class="w-full">
+    <div class="mx-auto my-2 flex w-full items-center justify-end">
+        <div class="mx-auto flex flex-col items-start justify-end md:w-4/5 md:flex-row md:px-5 lg:items-center">
+            <div class="flex w-full md:w-auto">
+                <span class="mx-4 w-1/2 self-end text-sm md:w-auto md:text-base">{{ __('txt.footer.project_by') }}</span>
+                {{-- <div class="flex flex-row justify-between"> --}}
+                <a class="ml-3" href="https://code4.ro/ro/code-for-romania-war-task-force">
+                    <img class="h-7 lg:h-14" src="{{ mix('assets/images/code4ro_head.png') }}" alt="Code4Romania logo">
+                </a>
+                <a class="ml-3" href="https://code4.ro/ro/code-for-romania-war-task-force">
+                    <img class="h-7 lg:h-14" src="{{ mix('assets/images/logo-smurd2.png') }}" alt="SMURD logo">
+                </a>
+                {{-- </div> --}}
+            </div>
+            <div class="mt-2 flex w-full md:mt-0 md:w-1/3 md:justify-end lg:w-1/5">
+
+                <span
+                    class="mx-4 w-1/2 self-end text-sm md:w-auto md:text-base">{{ __('txt.footer.supported_by') }}</span>
+                <a class="ml-3" href="https://code4.ro/ro/code-for-romania-war-task-force">
+                    <img class="h-7 lg:h-14" src="{{ mix('assets/images/lidl-logo.png') }}" alt="Lidl logo">
+                </a>
+            </div>
         </div>
     </div>
-    <div class="w-full bg-black text-white text-[15]">
-        <div class="container mx-auto flex h-80 w-4/5">
-            <div class="flex w-1/2 pb-3">
-                <div class="mt-6 flex w-1/3 flex-col justify-center pl-4">
-                    <h4 class="mb-4 text-base font-bold text-yellow">{{ __('txt.footer.util_links') }}</h4>
-                    <ul>
+    <div class="w-full bg-black text-sm text-white">
+        <div class="container mx-auto flex min-h-full flex-col p-5 lg:w-4/5 lg:flex-row lg:p-10">
+            <div class="flex w-full pb-3 lg:w-2/5">
+                <div class="flex w-1/2 flex-col items-center justify-start lg:items-start">
+                    <h4 class="mb-4 text-base font-bold text-yellow md:text-[15]">{{ __('txt.footer.util_links') }}</h4>
+                    <ul class="lg:text-base">
                         <li>
                             <a href="https://code4.ro/en/donate/">
                                 {{ __('txt.footer.donate') }}
@@ -35,9 +44,9 @@
                         </li>
                     </ul>
                 </div>
-                <div class="flex w-2/3 flex-col justify-center">
+                <div class="flex w-1/2 flex-col items-center justify-start lg:items-start">
                     <h4 class="mb-4 text-base font-bold text-yellow"> {{ __('txt.footer.legal_info') }}</h4>
-                    <ul>
+                    <ul class="lg:text-base">
                         <li>
                             <a href="https://code4.ro/en/donate/">
                                 {{ __('txt.footer.policy') }}
@@ -50,14 +59,14 @@
                     </ul>
                 </div>
             </div>
-            <div class="flex w-1/2 flex-col items-end justify-center pr-5">
-                <div class="mb-5 mt-4 w-32">
+            <div class="flex w-full flex-col items-end justify-center lg:w-3/5">
+                <div class="w-20 lg:my-4 lg:w-32">
                     <a href="/">
                         <img src="{{ mix('assets/images/commitglobal_white.png') }}" alt="Logo">
                     </a>
                 </div>
-                <h4>&#169; 2022 Commit Global</h4>
-                <h4> {{ __('txt.footer.commit_txt') }}</h4>
+                <h4 class="lg:text-base">&#169; 2022 Commit Global</h4>
+                <h4 class="lg:text-base"> {{ __('txt.footer.commit_txt') }}</h4>
             </div>
         </div>
     </div>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,20 +1,21 @@
 <header>
     <div class="container max-w-full">
         <div class="h-12 bg-bg-gray">
-            <div class="container mx-auto flex py-2 sm:px-6">
+            <div class="md:container md:mx-auto flex p-2">
                 <img class="h-8 w-28" src="{{ mix('assets/images/code4ro_head.png') }}">
-                <div class="ml-4 self-center font-['Roboto'] text-base">
-                    <span>{{ __('txt.header.code_for_romania') }}</span>
-                    <a class="ml-2 whitespace-nowrap text-blue-600" href="https://code4.ro/en/donate/" target="_blank"
+                <div class="font-['Roboto'] text-base md:ml-4 md:self-center">
+                    <span class="hidden md:inline">{{ __('txt.header.code_for_romania') }}</span>
+                    <a class="ml-2 whitespace-nowrap text-blue-600 " href="https://code4.ro/en/donate/" target="_blank"
                         rel="noopener">
                         {{ __('txt.buttons.find_more') }}
                     </a>
                 </div>
             </div>
         </div>
-        <div class="container mx-auto h-24 sm:px-4">
-            <div class="flex h-24 items-center">
-                <div class="w-40">
+
+        <div class="container mx-auto h-16 md:h-24">
+            <div class="flex  h-16 md:h-24 px-2 items-center justify-between">
+                <div class="w-20 md:w-40">
                     <a class="logo-box" href="{{ route('home') }}">
                         <img src="{{ mix('assets/images/logo.png') }}" alt="Logo">
                     </a>
@@ -29,36 +30,38 @@
                             ry="3" />
                     </svg>
                 </div>
-                <div class="flex w-full items-center justify-end" id="sideNav">
-                    <div class="flex items-center text-lg font-bold">
+                <div class="hidden md:flex w-full items-center justify-end" id="sideNav">
+                    <div class="flex items-center text-base lg:text-lg font-bold">
 
                         {{-- ToDo add routes to menu  --}}
 
-                        <a class="mr-10 text-lg font-bold" href="{{ route('home') }}"> {{ __('txt.header.home') }} </a>
-                        <a class="mr-10 text-lg font-bold" href="{{ route('home') }}">
+                        <a class="mr-5 lg:mr-10 " href="{{ route('home') }}"> {{ __('txt.header.home') }} </a>
+                        <a class="mr-5 lg:mr-10" href="{{ route('home') }}">
                             {{ __('txt.header.about_project') }} </a>
-                        <a class="mr-10 text-lg font-bold" href="{{ route('guide.show') }}"> {{ __('txt.header.guides') }}
+                        <a class="mr-5 lg:mr-10" href="{{ route('guide.show') }}">
+                            {{ __('txt.header.guides') }}
                         </a>
-                        <a class="mr-10 text-lg font-bold" href="{{ route('courses.index') }}"> {{ __('txt.header.courses') }}
+                        <a class="mr-5 lg:mr-10" href="{{ route('courses.index') }}">
+                            {{ __('txt.header.courses') }}
                         </a>
-                        <a class="mr-10 text-lg font-bold" href="{{ route('courses.index') }}">
+                        <a class="mr-5 lg:mr-10" href="{{ route('courses.index') }}">
                             {{ __('txt.header.contact') }} </a>
-                        <a class="mr-10 text-lg font-bold" href="{{ route('courses.index') }}">
+                        <a class="mr-5 lg:mr-10" href="{{ route('courses.index') }}">
                             {{ __('txt.header.sustain') }} </a>
                         @foreach ($pages as $page)
                             @if ($page->show_in_header)
                                 <a href="{{ route('page', $page) }}"> {{ $page->title }} </a>
                             @endif
                         @endforeach
-                        <a class="button flex h-10 w-32 bg-success-color uppercase"
-                            href="https://code4.ro/en/donate/"> {{ __('txt.buttons.donate') }} </a>
+                        <a class="button flex h-7 lg:h-10 w-20 lg:w-32 bg-success-color uppercase" href="https://code4.ro/en/donate/">
+                            {{ __('txt.buttons.donate') }} </a>
                     </div>
                 </div>
 
             </div>
         </div>
-        <div class="flex h-16 items-center justify-center bg-main-color">
-            <h4 class="text-3xl font-bold text-white"> {{ __('txt.header.warning_strip') }}</h4>
+        <div class="flex h-10 items-center justify-center bg-main-color md:h-16">
+            <h4 class="text-xl md:text-3xl font-bold text-white"> {{ __('txt.header.warning_strip') }}</h4>
         </div>
     </div>
 </header>
