145c7c940b283f7ff6387f3d5b2fbb28fe782d66
Vilen Bejenaru <70964449+vilenbejenaru@users.noreply.github.com>
2023-02-21 21:04:14 +0300

    responsive guides

--- resources/views/guides/show.blade.php
+++ resources/views/guides/show.blade.php
@@ -12,12 +12,13 @@
             </div>
         </section>
         <section class="flex-col justify-between" id="step1">
-            <h1 class="px-5">Cum procedez în caz de arsură?</h1>
-            <div class="card-step px-5 pt-10">
-                <div class="mb-10 flex justify-between">
-                    <div class="flex w-3/5 flex-col">
-                        <h2 class="w-2/3"> {{ __('txt.guide.step') }} 1</h2>
-                        <p class="w-4/5 text-2xl leading-9">Lorem Ipsum is simply dummy text of the printing and typesetting
+            <h1 class="w-full text-2xl md:w-2/3 md:text-4xl">Cum procedez în caz de arsură?</h1>
+            <div class="card-step flex flex-col px-5 pt-10 ">
+                <div class="mb-10 flex w-full flex-col justify-between md:flex-row">
+                    <div class="flex w-full flex-col md:w-3/5">
+                        <h2 class="w-full text-xl md:w-2/3 md:text-2xl"> {{ __('txt.guide.step') }} 1</h2>
+                        <p class="text-x d:w-4/5 w-full leading-6 md:text-2xl md:leading-9">Lorem Ipsum is simply dummy text
+                            of the printing and typesetting
                             industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an
                             unknown printer took a galley of type and scrambled it to make a type specimen book. It has
                             survived not only five centuries, but also the leap into electronic typesetting, remaining
@@ -26,89 +27,93 @@
                             PageMaker including versions of Lorem Ipsum.
                         </p>
                     </div>
-                    <div class="flex w-2/5 items-center justify-center">
+                    <div class="flex w-full items-center justify-center md:w-2/5">
                         <img src="{{ mix('assets/images/home_hero.png') }}" alt="prim ajutor">
                     </div>
                 </div>
-                <div class="mx-auto mb-10 flex w-4/5 justify-center">
-                    <button class="button w-2/5 bg-bg-gray mx-3"> {{ __('txt.buttons.prev_step') }}</button>
-                    <button class="button w-2/5 bg-yellow mx-3"> {{ __('txt.buttons.next_step') }}</button>
+                <div class="mx-auto mb-10 flex w-full justify-center md:w-4/5">
+                    <button class="button m-2 w-2/5 bg-bg-gray text-sm md:mx-3 md:text-base">
+                        {{ __('txt.buttons.prev_step') }}</button>
+                    <button class="button m-2 w-2/5 bg-yellow text-sm md:mx-3 md:text-base">
+                        {{ __('txt.buttons.next_step') }}</button>
                 </div>
             </div>
         </section>
         <section class="flex-col justify-between" id="step2">
-            <h1 class="px-5">Cum procedez în caz de arsură?</h1>
-            <div class="card-step px-5 pt-10">
-                <div class="mb-10 flex justify-between">
-                    <div class="flex w-3/5 flex-col">
-                        <h2 class="w-2/3"> {{ __('txt.guide.step') }} 2</h2>
-                        <p class="w-4/5 text-2xl leading-9">
+            <h1 class="w-full text-2xl md:w-2/3 md:text-4xl">Cum procedez în caz de arsură?</h1>
+            <div class="card-step flex flex-col px-5 pt-10 ">
+                <div class="mb-10 flex w-full flex-col justify-between md:flex-row">
+                    <div class="flex w-full flex-col md:w-3/5">
+                        <h2 class="w-full text-xl md:w-2/3 md:text-2xl"> {{ __('txt.guide.step') }} 2</h2>
+                        <p class="w-full md:w-4/5 text-xl leading-6 md:text-2xl md:leading-9">
                             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum.
                         </p>
-                        <ul class="ml-10 mb-5 w-4/5 list-disc text-xl leading-9">
+                        <ul class="ml-10 my-5 list-disc w-full md:w-4/5 text-base leading-6 md:text-xl md:leading-9">
                             <li>typesetting, remaining essentially</li>
                             <li>typesetting, remaining essentially</li>
                             <li>typesetting, remaining essentially</li>
                         </ul>
-                        <p class="w-4/5 text-2xl leading-9">
+                        <p class="w-full md:w-4/5 text-xl leading-6 md:text-2xl md:leading-9">
                             It has survived not only five centuries, but also the leap into electronic typesetting,
                             remaining essentially unchanged. It was popularised in the 1960s with.
                         </p>
                     </div>
-                    <div class="flex w-2/5 items-center justify-center">
+                    <div class="flex w-full items-center justify-center md:w-2/5">
                         <img src="{{ mix('assets/images/home_hero.png') }}" alt="prim ajutor">
                     </div>
                 </div>
-                <div class="mx-auto mb-10 flex w-4/5 justify-center">
-                    <button class="button w-2/5 bg-bg-gray mx-3"> {{ __('txt.buttons.prev_step') }}</button>
-                    <button class="button w-2/5 bg-yellow mx-3"> {{ __('txt.buttons.next_step') }}</button>
+                <div class="mx-auto mb-10 flex w-full justify-center md:w-4/5">
+                    <button class="button m-2 w-2/5 bg-bg-gray text-sm md:mx-3 md:text-base">
+                        {{ __('txt.buttons.prev_step') }}</button>
+                    <button class="button m-2 w-2/5 bg-yellow text-sm md:mx-3 md:text-base"> {{ __('txt.buttons.next_step') }}</button>
                 </div>
             </div>
         </section>
-        <section class="flex-col justify-between" id="step3">
-            <h1 class="px-5">Cum procedez în caz de arsură?</h1>
-            <div class="card-step px-5 pt-10">
-                <div class="mb-10 flex justify-between">
-                    <div class="flex w-3/5 flex-col">
-                        <h2 class="w-2/3"> {{ __('txt.guide.step') }} 3</h2>
-                        <p class="w-4/5 text-2xl leading-9">
+        <section class="flex-col justify-between" id="step2">
+            <h1 class="w-full text-2xl md:w-2/3 md:text-4xl">Cum procedez în caz de arsură?</h1>
+            <div class="card-step flex flex-col px-5 pt-10 ">
+                <div class="mb-10 flex w-full flex-col justify-between md:flex-row">
+                    <div class="flex w-full flex-col md:w-3/5">
+                        <h2 class="w-full text-xl md:w-2/3 md:text-2xl"> {{ __('txt.guide.step') }} 3</h2>
+                        <p class="w-full md:w-4/5 text-xl leading-6 md:text-2xl md:leading-9">
                             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum.
                         </p>
-                        <ul class="ml-10 mb-5 w-4/5 list-disc text-xl leading-9">
+                        <ul class="ml-10 my-5 list-disc w-full md:w-4/5 text-base leading-6 md:text-xl md:leading-9">
                             <li>typesetting, remaining essentially</li>
                             <li>typesetting, remaining essentially</li>
                             <li>typesetting, remaining essentially</li>
                         </ul>
-                        <p class="w-4/5 text-2xl leading-9">
+                        <p class="w-full md:w-4/5 text-xl leading-6 md:text-2xl md:leading-9">
                             It has survived not only five centuries, but also the leap into electronic typesetting,
                             remaining essentially unchanged. It was popularised in the 1960s with.
                         </p>
                     </div>
-                    <div class="flex w-2/5 items-center justify-center">
+                    <div class="flex w-full items-center justify-center md:w-2/5">
                         <img src="{{ mix('assets/images/home_hero.png') }}" alt="prim ajutor">
                     </div>
                 </div>
-                <div class="mx-auto mb-10 flex w-4/5 justify-center">
-                    <button class="button w-2/5 bg-bg-gray mx-3"> {{ __('txt.buttons.prev_step') }}</button>
-                    <button class="button w-2/5 bg-yellow mx-3"> {{ __('txt.buttons.back_first') }}</button>
+                <div class="mx-auto mb-10 flex w-full justify-center md:w-4/5">
+                    <button class="button m-2 w-2/5 bg-bg-gray text-sm md:mx-3 md:text-base">
+                        {{ __('txt.buttons.prev_step') }}</button>
+                    <button class="button m-2 w-2/5 bg-yellow text-sm md:mx-3 md:text-base"> {{ __('txt.buttons.next_step') }}</button>
                 </div>
             </div>
         </section>
         <section class="flex-col" id="other_guides">
             <hr class="mt-10 w-full bg-black">
-            <h2 class="mt-8 text-4xl">{{ __('txt.guide.other_guides') }}</h2>
-            <div class="flex flex-wrap justify-between">
-                <div class="card min-h-min flex-col items-center justify-start">
+            <h2  class="w-full text-2xl md:w-2/3 md:text-4xl mt-8">{{ __('txt.guide.other_guides') }}</h2>
+            <div class="flex flex-col md:flex-row md:flex-wrap md:justify-evenly xl:justify-between">
+               <div class="card min-h-min w-full md:w-2/5 lg:w-1/4 lg:mx-1  flex-col items-center justify-start">
                     <img class="w-1/3" src="{{ mix('assets/images/guide_card_logo.png') }}" alt="guide">
                     <h4 class="title">Stopul
                         cardio-respirator </h4>
                 </div>
-                <div class="card min-h-min flex-col items-center justify-start">
+               <div class="card min-h-min w-full md:w-2/5 lg:w-1/4 lg:mx-1  flex-col items-center justify-start">
                     <img class="w-1/3" src="{{ mix('assets/images/guide_card_logo.png') }}" alt="guide">
                     <h4 class="title">Persoană
                         inconștientă</h4>
                 </div>
-                <div class="card min-h-min flex-col items-center justify-start">
+               <div class="card min-h-min w-full md:w-2/5 lg:w-1/4 lg:mx-1  flex-col items-center justify-start">
                     <img class="w-1/3" src="{{ mix('assets/images/guide_card_logo.png') }}" alt="guide">
                     <h4 class="title">Șoc
                         anafilactic</h4>
@@ -118,49 +123,49 @@
         </section>
         <section class="flex-col" id="list_guides">
             <hr class="mt-10 w-full bg-black">
-            <h2 class="mt-8 text-4xl">{{ __('txt.guide.title_list_guides') }}</h2>
-            <div class="flex flex-wrap justify-between">
-                <div class="card min-h-min flex-col items-center justify-start">
+            <h2  class="w-full text-2xl md:w-2/3 md:text-4xl mt-8">{{ __('txt.guide.title_list_guides') }}</h2>
+            <div class="flex flex-col md:flex-row md:flex-wrap md:justify-evenly xl:justify-between">
+               <div class="card min-h-min w-full md:w-2/5 lg:w-1/4 lg:mx-1  flex-col items-center justify-start">
                     <img class="w-1/3" src="{{ mix('assets/images/guide_card_logo.png') }}" alt="guide">
                     <h4 class="title">Stopul
                         cardio-respirator </h4>
                 </div>
-                <div class="card min-h-min flex-col items-center justify-start">
+               <div class="card min-h-min w-full md:w-2/5 lg:w-1/4 lg:mx-1  flex-col items-center justify-start">
                     <img class="w-1/3" src="{{ mix('assets/images/guide_card_logo.png') }}" alt="guide">
                     <h4 class="title">Persoană
                         inconștientă</h4>
                 </div>
-                <div class="card min-h-min flex-col items-center justify-start">
+               <div class="card min-h-min w-full md:w-2/5 lg:w-1/4 lg:mx-1  flex-col items-center justify-start">
                     <img class="w-1/3" src="{{ mix('assets/images/guide_card_logo.png') }}" alt="guide">
                     <h4 class="title">Șoc
                         anafilactic</h4>
                 </div>
-                <div class="card min-h-min flex-col items-center justify-start">
+               <div class="card min-h-min w-full md:w-2/5 lg:w-1/4 lg:mx-1  flex-col items-center justify-start">
                     <img class="w-1/3" src="{{ mix('assets/images/guide_card_logo.png') }}" alt="guide">
                     <h4 class="title">Stopul
                         cardio-respirator </h4>
                 </div>
-                <div class="card min-h-min flex-col items-center justify-start">
+               <div class="card min-h-min w-full md:w-2/5 lg:w-1/4 lg:mx-1  flex-col items-center justify-start">
                     <img class="w-1/3" src="{{ mix('assets/images/guide_card_logo.png') }}" alt="guide">
                     <h4 class="title">Persoană
                         inconștientă</h4>
                 </div>
-                <div class="card min-h-min flex-col items-center justify-start">
+               <div class="card min-h-min w-full md:w-2/5 lg:w-1/4 lg:mx-1  flex-col items-center justify-start">
                     <img class="w-1/3" src="{{ mix('assets/images/guide_card_logo.png') }}" alt="guide">
                     <h4 class="title">Șoc
                         anafilactic</h4>
                 </div>
-                <div class="card min-h-min flex-col items-center justify-start">
+               <div class="card min-h-min w-full md:w-2/5 lg:w-1/4 lg:mx-1  flex-col items-center justify-start">
                     <img class="w-1/3" src="{{ mix('assets/images/guide_card_logo.png') }}" alt="guide">
                     <h4 class="title">Stopul
                         cardio-respirator </h4>
                 </div>
-                <div class="card min-h-min flex-col items-center justify-start">
+               <div class="card min-h-min w-full md:w-2/5 lg:w-1/4 lg:mx-1  flex-col items-center justify-start">
                     <img class="w-1/3" src="{{ mix('assets/images/guide_card_logo.png') }}" alt="guide">
                     <h4 class="title">Persoană
                         inconștientă</h4>
                 </div>
-              
+
             </div>
             </div>
         </section>
